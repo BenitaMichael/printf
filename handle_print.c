29314ec092849c44f348cb1203697a3cc6f3f640
BenitaMichael <benubiaka@gmail.com>
2023-04-22 14:48:15 -0700

    errors

--- handle_print.c
+++ handle_print.c
@@ -7,16 +7,15 @@
  * @ind: index
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
- * @width: width
+ * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-	int i, unknown_lenght = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -27,13 +26,14 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_lenght += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknown_lenght += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -43,8 +43,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unknown_lenght += write(1, &fmt[*ind], 1);
-		return (unknown_lenght);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
