ff38a84f804cb2f089b4fd084508c623693a2508
BenitaMichael <benubiaka@gmail.com>
2023-04-22 14:20:44 -0700

    Error in function fixed

--- function1.c
+++ function1.c
@@ -114,19 +114,20 @@ int print_hexa_upper(va_list types, char buffer[],
 }
 
 /************** PRINT HEXX NUM IN LOWER OR UPPER **************/
-OB/**
-OB * print_hexa - Prints a hexadecimal number in lower or upper
-OB * @types: Lista of arguments
+/**
+ * print_hexa - Prints a hexadecimal number in lower or upper
+ * @types: Lista of arguments
  * @map_to: Array of values to map the number to
-OB * @buffer: Buffer array to handle print
-OB * @flags:  Calculates active flags
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
  * @flag_ch: Calculates active flags
  * @width: get width
-OB * @precision: Precision specification
+ * @precision: Precision specification
  * @size: Size specifier
  * @size: Size specification
  * Return: Number of chars printed
  */
+
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
