cfedac08fb5d67665754b471230e9e4108218822
BenitaMichael <benubiaka@gmail.com>
2023-04-23 16:21:54 -0700

    prerequisite

--- get_flags.c
+++ get_flags.c
@@ -8,15 +8,16 @@
  */
 int get_flags(const char *format, int *i)
 {
-	int a, curr_i;
+	int a;
+	int current_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
 	{
 		for (a = 0; FLAGS_CH[a] != '\0'; a++)
-			if (format[curr_i] == FLAGS_CH[a])
+			if (format[current_i] == FLAGS_CH[a])
 			{
 				flags |= FLAGS_ARR[a];
 				break;
@@ -26,7 +27,7 @@ int get_flags(const char *format, int *i)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = current_i - 1;
 
 	return (flags);
 }
