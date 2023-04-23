b47bfdceebab1dfc324ab1cc5c6c56cd27b49f7d
BenitaMichael <benubiaka@gmail.com>
2023-04-23 15:30:30 -0700

    prerequisite

--- get_width.c
+++ get_width.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: width.
+ * get_width - Calculate the width for printing outputs
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ * Return: returns width
  */
 int get_width(const char *format, int *i, va_list list)
 {
