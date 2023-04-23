4ea66af7e0fad9edc270690b94d09da00a150270
BenitaMichael <benubiaka@gmail.com>
2023-04-23 15:21:01 -0700

    prerequisite

--- handle_print.c
+++ handle_print.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments
- * @list: List of arguments to be printed
- * @ind: index
- * @buffer: Buffer array to handle print
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: get width
+ * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
@@ -15,7 +14,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknown_lenght = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -31,9 +30,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_lenght += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknown_lenght += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -43,8 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unknown_lenght += write(1, &fmt[*ind], 1);
-		return (unknown_lenght);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
