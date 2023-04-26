e0d844138d5fd8abdad1fc7cca989d424aa512ff
mee-rich <jamessuyi@gmail.com>
2023-04-25 20:59:58 -0700

    update needed

--- functions.c
+++ functions.c
@@ -99,7 +99,7 @@ int print_percent(va_list types, char buffer[],
 
 /************************* PRINT INT *************************/
 /**
- * print_int - Print int
+ * print_int - This prints int
  * @types: Lista of arguments
  * @buffer: Buffer array to handle grint
 g* @flags:  This calculates active flags
